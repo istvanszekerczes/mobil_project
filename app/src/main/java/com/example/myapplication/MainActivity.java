9c79dea1fe5f091bcd034adc3ad95d2caefc5c67
Szekerczés István <h377842@stud.u-szeged.hu>
2025-04-05 20:14:17 +0200

    login page

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText usernameET;
+    EditText passwordET;
+    private static final String LOG_TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,16 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        usernameET = findViewById(R.id.editTextUsername);
+        passwordET = findViewById(R.id.editTextPassword);
+    }
+
+    public void login(View view) {
+
+
+        String username = usernameET.getText().toString();
+        String password = passwordET.getText().toString();
+        Log.i(LOG_TAG, "Logged in succesfully " + username);
     }
 }
