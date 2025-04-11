e724b3a843a5d03506e15d28dc7476e04600629e
Szekerczés István <h377842@stud.u-szeged.hu>
2025-04-11 20:08:50 +0200

    starting the backend

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,8 +10,11 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    EditText usernameET;
-    EditText passwordET;
+    EditText usernameEditText;
+    EditText passwordEditText;
+
+    private SharedPreferences sharedPreferences;
+    private static final String PREF_KEY = MainActivity.class.getPackage().toString();
     private static final String LOG_TAG = MainActivity.class.getName();
     private static final int SECRET_KEY = 99;
 
@@ -19,15 +23,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        usernameET = findViewById(R.id.editTextUsername);
-        passwordET = findViewById(R.id.editTextPassword);
+        usernameEditText = findViewById(R.id.editTextUsername);
+        passwordEditText = findViewById(R.id.editTextPassword);
+
+        Log.i(LOG_TAG, "onCreate");
+
+        sharedPreferences = getSharedPreferences(PREF_KEY, MODE_PRIVATE);
+
+
     }
 
     public void login(View view) {
 
 
-        String username = usernameET.getText().toString();
-        String password = passwordET.getText().toString();
+        String username = usernameEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
         Log.i(LOG_TAG, "Logged in succesfully " + username);
 
         //TODO: Login befejezese
@@ -38,5 +48,45 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("SECRET_KEY", SECRET_KEY);
         startActivity(intent);
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("username", usernameEditText.getText().toString());
+        editor.apply();
+
+        Log.i(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(LOG_TAG, "onResume ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(LOG_TAG, "onRestart ");
     }
 }
--- app/src/main/java/com/example/myapplication/SignUpActivity.java
+++ app/src/main/java/com/example/myapplication/SignUpActivity.java
@@ -1,15 +1,22 @@
 package com.example.myapplication;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class SignUpActivity extends AppCompatActivity {
+    EditText emailEditText;
+    EditText usernameEditText;
+    EditText passwordEditText;
+    EditText passwordAgainEditText;
+    private SharedPreferences sharedPreferences;
+    private static final String PREF_KEY = MainActivity.class.getPackage().toString();
+    private static final String LOG_TAG = SignUpActivity.class.getName();
+    private static final int SECRET_KEY = 99;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,31 @@ public class SignUpActivity extends AppCompatActivity {
         if (secretKey != 99) {
             finish();
         }
+        emailEditText = findViewById(R.id.editTextEmail);
+        usernameEditText = findViewById(R.id.editTextUsername);
+        passwordEditText = findViewById(R.id.editTextPassword);
+        passwordAgainEditText = findViewById(R.id.editTextPasswordAgain);
+
+        sharedPreferences = getSharedPreferences(PREF_KEY, MODE_PRIVATE);
+        String username =  sharedPreferences.getString("username", "");
+
+        usernameEditText.setText(username);
+        
+
+        Log.i(LOG_TAG, "onCreate");
+    }
+    public void signUp(View view) {
+        String email = emailEditText.getText().toString();
+        String username = usernameEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String passwordAgain = passwordAgainEditText.getText().toString();
+
+        if (!password.equals(passwordAgain)) {
+            Log.e(LOG_TAG, "Passwords do not match!");
+            return;
+        }
+
+        Log.i(LOG_TAG, "Signed up succesfully " + username);
 
 
 
@@ -29,6 +61,39 @@ public class SignUpActivity extends AppCompatActivity {
         finish();
     }
 
-    public void signUp(View view) {
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(LOG_TAG, "onResume ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(LOG_TAG, "onRestart ");
     }
 }
